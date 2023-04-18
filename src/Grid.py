51ce886abdf1b7576dec69cacc1932b0e2055bdf
Marble879 <younisakel879@gmail.com>
2023-04-18 14:46:09 +0300

    Adjust grid and main to run algorithm for creating permutations
    Co-authored-by: iachella <gusiacgi@student.gu.se>

--- src/Grid.py
+++ src/Grid.py
@@ -1,14 +1,18 @@
 from boundaryGenerator import setup_boundary_grid
+from boundaryGenerator import setup_boundary_grid_row_col
 
 class Grid:
 
     grid = None
     xAxis = None
     yAxis = None 
+    permutations = None
+    convertedPermutations = None
 
 
-    def __init__(self, minX, maxX, minY, maxY, xRes, yRes):
-        self.grid, self.xAxis, self.yAxis = setup_boundary_grid(minX, maxX, minY, maxY, xRes, yRes, output=False)
+    def __init__(self, minX, maxX, minY, maxY, rows, cols, dp):
+        #self.grid, self.xAxis, self.yAxis = setup_boundary_grid(minX, maxX, minY, maxY, xRes, yRes, output=False)
+        self.grid, self.xAxis, self.yAxis = setup_boundary_grid_row_col(minX, maxX, minY, maxY, rows, cols, dp, output=False)
 
     
     def get_grid(self):
@@ -19,3 +23,21 @@ class Grid:
 
     def get_yAxis(self):
         return self.yAxis
+
+    def set_permutations(self, permutations):
+        self.permutations = permutations
+
+    def get_permutations(self):
+        return self.permutations
+
+    def set_converted_permutations(self, convertedPermutations):
+        self.convertedPermutations = convertedPermutations
+        print(self.permutations)
+        print("--------------------------------------")
+        print(self.convertedPermutations)
+        print("====================================")
+
+    def get_converted_permutations(self):
+        return self.convertedPermutations
+
+    
--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from Grid import Grid
 from GridTest import GridTest
-from algorithm import *
+import numpy as np
+#from algorithm import *
 from PermutationVerifier import *
-#from algorithm import create_bell_shape
+from algorithm import generate_combinations_right_bell_recursion
+from algorithm import generate_combinations_left_bell_recursion
+from algorithm import generate_straight
+from algorithm import generate_boxes_permutations
+from PermutationConverter import *
 
 '''
 PSEUDOCODE FOR ALGORITHM:
@@ -34,10 +39,45 @@ can use a set to see if indexes of array respect the rules or not.
 
 '''
 def main():
-    #gridA = Grid(minX=1.6, maxX=1.62, minY=0, maxY=0.18, xRes=0.001, yRes=0.01) 
-    #gridB = Grid(minX=1.05, maxX=1.55, minY=0.19, maxY=0.47, xRes=0.001, yRes=0.01)
-    #gridC = Grid(minX=1.63, maxX=2.85, minY=0.48, maxY=0.76, xRes=0.001, yRes=0.01)
+    #gridA = Grid(minX=1.6, maxX=1.62, minY=0, maxY=0.18, xRes=0.01, yRes=0.01) # rows: 2 (0.0 -> 0.1), cols: 1 (1.6) #TODO (can incldue in thesis text if have to expalin graph): start 0.0 as its artificial time range. Ie lane change happens after certain time has occured, we just cut out as close to what we consider lane change
+    #gridB = Grid(minX=1.05, maxX=1.55, minY=0.19, maxY=0.47, xRes=0.01, yRes=0.01) # rows: 3, cols: 5 (0.2 -> 0.5)
+    #gridC = Grid(minX=1.63, maxX=2.85, minY=0.48, maxY=0.76, xRes=0.01, yRes=0.01)
     #gridD = Grid(minX=1.6, maxX=1.62, minY=0.77, maxY=0.95, xRes=0.01, yRes=0.01)
+
+    gridA = Grid(minX=1.6, maxX=1.6, minY=0, maxY=0.1, rows=2, cols=1, dp=1) # rows: 2 (0.0 -> 0.1), cols: 1 (1.6) #TODO (can incldue in thesis text if have to expalin graph): start 0.0 as its artificial time range. Ie lane change happens after certain time has occured, we just cut out as close to what we consider lane change
+    gridB = Grid(minX=1.1, maxX=1.5, minY=0.2, maxY=0.4, rows=3, cols=5, dp=1) # rows: 3, cols: 5 (0.2 -> 0.5)
+    gridC = Grid(minX=1.7, maxX=2.8, minY=0.5, maxY=0.7, rows=3, cols=12, dp=1) # rows: 3, cols: 12
+    gridD = Grid(minX=1.6, maxX=1.6, minY=0.8, maxY=0.9, rows=2, cols=1, dp=1) # rows: 2, cols: 1
+
+    print(gridA.get_xAxis() , "|", gridA.get_yAxis())
+    print(gridB.get_xAxis(), "|", gridB.get_yAxis())
+    print(gridC.get_xAxis(), "|", gridC.get_yAxis())
+    print(gridD.get_xAxis(), "|", gridD.get_yAxis())
+    
+
+    # run permutations for each red box
+    gridA.set_permutations(generate_straight(gridA.grid))
+    gridB.set_permutations(generate_combinations_left_bell_recursion(gridB.grid))
+    gridC.set_permutations(generate_combinations_right_bell_recursion(gridC.grid))
+    gridD.set_permutations(generate_straight(gridD.grid))
+
+    # convert permutations to (morton, ts)
+    gridA.set_converted_permutations(convert_permutations(gridA))
+    gridB.set_converted_permutations(convert_permutations(gridB))
+    gridC.set_converted_permutations(convert_permutations(gridC))
+    gridD.set_converted_permutations(convert_permutations(gridD))
+
+    # permutate all permutations of all boxes
+    boxResult = generate_boxes_permutations(gridA.convertedPermutations, gridB.convertedPermutations, gridC.convertedPermutations, gridD.convertedPermutations)
+
+    #print(boxResult)
+    print(len(gridA.convertedPermutations))
+    print(len(gridB.convertedPermutations))
+    print(len(gridC.convertedPermutations))
+    print(len(gridD.convertedPermutations))
+    print("Lane changes: ", len(boxResult))
+
+
 
     #TODO: BUG WHEN TRYING THE FOLLOWING (X-AXIS OUTPUT IS [1.05, 1.2, 1.2, 1.4, 1.5, 1.55]):
     #gridTest = Grid(minX=1.05, maxX=1.55, minY=0.19, maxY=0.47, xRes=0.10, yRes=0.06)
@@ -50,13 +90,33 @@ def main():
 ######### Algorithm TESTS ##############
     # Algorithm test
     # generate combinations for a 2x2 array
-    rows = 3
-    cols = 2
-    grid = np.zeros((rows, cols), dtype=int)
-    result = generate_combinations_recursion(grid)
-    print(result)
+    #rows = 8
+    #cols = 8
+    #grid = np.zeros((rows, cols), dtype=int)
+    #result = generate_combinations_left_bell_recursion(grid)
+    #print(result)
+
+    #rows = 3
+   # cols = 2
+   # grid = np.zeros((rows, cols), dtype=int)
+    #result = generate_combinations_right_bell_recursion(grid)
+    #print(result)
+
+    # TEST GENERATE PERMUTATIONS
+  #  boxA = [[(0, 0), (0, 2)], [(1, 0), (1, 2)]]
+   # boxB = [[(4, 5), (5, 4)], [(3, 4), (4, 3)]]
+   # boxC = [[(6, 7), (7, 6)], [(8, 9), (9, 8)]]
+    #boxD = [[(10, 11), (11, 10)], [(12, 13), (13, 12)]]
+    
+   # boxResult = generate_boxes_permutations(boxA, boxB, boxC, boxD)
+   # print ("boxes result\n", boxResult)
+
+    
+
+
 
     ######### RULE TESTS ##############
+    ###### LEFT BELL #######
     #### 5x5####
     #ruleTestData = [[(4, 0), (3, 1), (2, 2), (3, 3), (4, 4)]] #VALID
     #ruleTestData = [[(4, 0), (3, 1), (3, 2), (3, 3), (4, 4)]] #VALID
@@ -94,14 +154,23 @@ def main():
     [(3, 0), (3, 1), (3, 2), (3, 3), (4, 4)]
     ]
     '''
+    ###### RIGHT BELL #######
+    #ruleTestData = [[(0, 0), (1, 1), (0, 2)]] #valid
+    #ruleTestData = [[(4, 0), (10, 1), (0, 2)]] # valid 
+    #ruleTestData = [[(6, 0), (8, 1), (1, 2)]] # valid 
+    #ruleTestData = [[(6, 0), (8, 1), (11, 2)]] # invalid 
+    #ruleTestData = [[(0, 0), (1, 1), (2, 2)]] # invalid
+    #ruleTestData = [[(0, 0), (0, 1), (0, 2)]] # invalid
+    #ruleTestData = [[(3, 0), (4, 1), (4, 2)]] # invalid
+    #ruleTestData = [[(5, 0), (4, 1), (5, 2)]] # invalid
 
-   # print(len(ruleTestData))
+    #print(len(ruleTestData))
       # Filter sets to only contain sets that follows the right lane change (first C shape) definition
     #cleaned = []
     #for combination in ruleTestData:
-    #    if verify_permutation(combination):
+    #    if verify_permutation_right_bell(combination):
     #       cleaned.append(combination)
-   # print(len(cleaned))
+    #print(len(cleaned))
 # TODO: Make grid creation dynamic (younis)
 # TODO: Max bell shaped curve rules (gimmmmmmmy)
 # TODO: make straight drive perm rules
